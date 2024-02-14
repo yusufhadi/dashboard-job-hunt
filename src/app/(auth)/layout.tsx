ffe26fec096099ca0c3a8bae83b6a7414b9a3b2d
yusufhadi <hadiyusuf889@gmail.com>
2024-02-14 11:48:53 +0800

    integration sign-in

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,14 +4,23 @@ import type { Metadata } from "next";
 import { Epilogue } from "next/font/google";
 import Header from "@/components/layouts/Header";
 import { Toaster } from "@/components/ui/toaster";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
 const epilogue = Epilogue({ subsets: ["latin"] });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
+  if (session !== null) {
+    return redirect("/");
+  }
+
   return (
     <html lang="en">
       <body className={epilogue.className}>
---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,67 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "../../../../../lib/prisma";
+import { comparePassword } from "@/lib/utils";
+
+export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "email",
+          type: "email",
+        },
+        password: {
+          label: "password",
+          type: "password",
+        },
+      },
+      async authorize(credentials, req) {
+        const user = await prisma.company.findFirst({
+          where: {
+            email: credentials?.email,
+          },
+        });
+
+        if (!user) {
+          return null;
+        }
+
+        const isMatch = await comparePassword(
+          credentials?.password!!,
+          user.password
+        );
+
+        if (isMatch) {
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+    newUser: "/auth/signup",
+  },
+  callbacks: {
+    jwt({ token, account, user }) {
+      if (account) {
+        token.id = user.id;
+      }
+
+      return token;
+    },
+    async session({ session, token, user }) {
+      session.user.id = token.id;
+
+      return token;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
